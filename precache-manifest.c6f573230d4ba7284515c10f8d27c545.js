a720c12e632f9aa6d078412a8ba11d6fb72bf587
Rodrigo Alfonso <rodri042@gmail.com>
2023-09-25 08:20:50 -0300

    Deploy @ 25/09/2023

---
+++ precache-manifest.c6f573230d4ba7284515c10f8d27c545.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "4ee1879da8a4ab7629a6",
+    "url": "/jeringozo/static/css/main.199c5d2a.chunk.css"
+  },
+  {
+    "revision": "4ee1879da8a4ab7629a6",
+    "url": "/jeringozo/static/js/main.c4205208.chunk.js"
+  },
+  {
+    "revision": "4c67ae2503ac941cced8",
+    "url": "/jeringozo/static/js/runtime~main.a05c9e1d.js"
+  },
+  {
+    "revision": "5358e1f20ef4483e3fb1",
+    "url": "/jeringozo/static/js/2.a6c480a1.chunk.js"
+  },
+  {
+    "revision": "197635266729fae071d49309b75abb20",
+    "url": "/jeringozo/index.html"
+  }
+];
